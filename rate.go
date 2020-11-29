5a1d7abfc62e4219f7951d727d07f1cec9469c2e
alextanhongpin <alextan220990@gmail.com>
2020-11-29 17:27:01 +0800

    chore: remove redundant round method

--- rate.go
+++ rate.go
@@ -56,19 +56,5 @@ func Hot(upvotes, downvotes int64, date time.Time) float64 {
 	epoch := time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC).UnixNano() / 1e6
 	// epoch_seconds := time.Date(1970,1,14, 3, 0, 28, 3e6, time.UTC).UnixNano() / 1e6
 	seconds := (date.UnixNano()/1e6-epoch)/1e3 - 1134028003
-	return round(sign*order+float64(seconds)/45000.0, 0.5, 7)
-}
-
-func round(val float64, roundOn float64, places int) (newVal float64) {
-	var round float64
-	pow := math.Pow(10, float64(places))
-	digit := pow * val
-	_, div := math.Modf(digit)
-	if div >= roundOn {
-		round = math.Ceil(digit)
-	} else {
-		round = math.Floor(digit)
-	}
-	newVal = round / pow
-	return
+	return sign*order + float64(seconds)/45000.0
 }
